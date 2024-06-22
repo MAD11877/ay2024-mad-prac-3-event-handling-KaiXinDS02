ca13aa9c9969aa8c89ee37768803e5485b01e730
Kai Xin <s10255744@connect.np.edu.sg>
2024-06-22 13:57:35 +0800

    Final

--- app/src/main/java/sg/edu/np/mad/madpractical2/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/User.java
@@ -9,21 +9,52 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class User {
+    private String name;
+    private String description;
+    private int id;
+    private boolean followed;
 
-    private final String descriptiom;
-    public String name;
-
-    public String description;
-
-    public int id;
-
-    public boolean followed;
-
-    public User(String name, String description, int id, Boolean followed ){
+    // Constructor
+    public User(String name, String description, int id, boolean followed) {
         this.name = name;
-        this.descriptiom = description;
+        this.description = description;
         this.id = id;
         this.followed = followed;
     }
 
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for followed
+    public boolean isFollowed() {
+        return followed;
+    }
+
+    public void setFollowed(boolean followed) {
+        this.followed = followed;
+    }
 }
